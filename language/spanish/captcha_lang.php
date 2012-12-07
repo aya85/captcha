6cf0c0085fb02534198a4cb4d4c10bfe05e6aa79
paulino <paulino@alifanfaron>
2012-12-07 23:31:40 +0100

    Added spanish language

---
+++ language/spanish/captcha_lang.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams.captcha.name']           = 'CAPTCHA';
+$lang['streams.captcha.img_width']              = 'Ancho de la Imagen';
+$lang['streams.captcha.img_height']              = 'Altura de la Imagen';
+$lang['streams.captcha.invalid']        = 'El valor no coincide con el del texto en la imagen';
